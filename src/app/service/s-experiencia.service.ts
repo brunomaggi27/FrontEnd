34085ab5d7830cc71536ffdf353ad76156792b05
brunomaggi27 <bmaggi@outlook.com>
2023-06-29 21:23:59 -0300

    q1
    q1

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -10,7 +10,7 @@ import { Experiencia } from '../model/experiencia';
 })
 
 export class SExperienciaService {
-  expURL = 'http://localhost:8080/explab/'
+  expURL = 'hhttps://backendbm.onrender.com/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
